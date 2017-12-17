197cf0d45dc95a9a4423d0a9e32a38d8a944a963
vadmurzakov <vadmurzakov@gmail.com>
2017-12-17 04:39:55 +0300

    #2 VkService#getWallpost

--- src/main/java/ru/rnemykin/newsbot/service/VkService.java
+++ src/main/java/ru/rnemykin/newsbot/service/VkService.java
@@ -6,6 +6,9 @@ import com.vk.api.sdk.client.actors.ServiceActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.groups.GroupFull;
+import com.vk.api.sdk.objects.wall.WallpostFull;
+import com.vk.api.sdk.objects.wall.responses.GetResponse;
+import com.vk.api.sdk.queries.wall.WallGetFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,7 @@ import ru.rnemykin.newsbot.config.vkontakte.VkConfiguration;
 import ru.rnemykin.newsbot.model.enums.PublicEnum;
 
 import javax.annotation.PostConstruct;
+import java.text.MessageFormat;
 import java.util.List;
 
 @Service
@@ -20,6 +24,9 @@ import java.util.List;
 public class VkService {
 
 	@Autowired private VkConfiguration configuration;
+
+	private final static Integer COUNT_WALLPOST = 5;
+	private final static Integer OFFSET_WALLPOST = 0;
 
 	private ServiceActor actor;
 	private VkApiClient vk;
@@ -39,8 +46,33 @@ public class VkService {
 			List<GroupFull> groupFulls = vk.groups().getById(actor).groupId(id.toString()).execute();
 			return Iterables.getFirst(groupFulls, null);
 		} catch (ApiException | ClientException e) {
-			log.error("Error get group " + PublicEnum.fromId(id));
+			log.error(MessageFormat.format("Error get group {0}, {1}", PublicEnum.fromId(id), e.getMessage()));
 			return null;
 		}
+	}
+
+	public List<WallpostFull> getWallpost(Long groupId) {
+		return getWallpost(calcGroupId(groupId), COUNT_WALLPOST, OFFSET_WALLPOST);
+	}
+
+	public List<WallpostFull> getWallpost(GroupFull group) {
+		return getWallpost(calcGroupId(group.getId()), COUNT_WALLPOST, OFFSET_WALLPOST);
+	}
+
+	public List<WallpostFull> getWallpost(Integer groupId, Integer count, Integer offset) {
+		try {
+			GetResponse response = vk.wall().get(actor).ownerId(calcGroupId(groupId)).filter(WallGetFilter.OWNER).count(count).offset(offset).execute();
+			return response.getItems();
+		} catch (ApiException | ClientException e) {
+			log.error(MessageFormat.format("Error get wall posts for group {0}, {1}", PublicEnum.fromId(groupId.longValue()), e.getMessage()));
+			return null;
+		}
+	}
+
+	/**
+	 * @return group id must be negative
+	 */
+	private Integer calcGroupId(Object groupId) {
+		return new Integer(String.valueOf(groupId)) * -1;
 	}
 }
--- src/test/java/ru/rnemykin/newsbot/service/VkServiceTest.java
+++ src/test/java/ru/rnemykin/newsbot/service/VkServiceTest.java
@@ -1,12 +1,15 @@
 package ru.rnemykin.newsbot.service;
 
 import com.vk.api.sdk.objects.groups.GroupFull;
+import com.vk.api.sdk.objects.wall.WallpostFull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.rnemykin.newsbot.model.enums.PublicEnum;
+
+import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -20,5 +23,18 @@ public class VkServiceTest {
 	public void getGroup() {
 		GroupFull group = service.getGroup(PublicEnum.BEELIVE.id());
 		assertNotNull(group);
+	}
+
+	@Test
+	public void getWallpostByGroupId() {
+		List<WallpostFull> wallpost = service.getWallpost(PublicEnum.BEELIVE.id());
+		assertNotNull(wallpost);
+	}
+
+	@Test
+	public void getWallpostByGroup() {
+		GroupFull group = service.getGroup(PublicEnum.BEELIVE.id());
+		List<WallpostFull> wallpost = service.getWallpost(group);
+		assertNotNull(wallpost);
 	}
 }
