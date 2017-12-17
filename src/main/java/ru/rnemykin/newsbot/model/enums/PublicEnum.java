37cf76ac3e18def4b19e3d44253eb48600bd265e
vadmurzakov <vadmurzakov@gmail.com>
2017-12-17 03:46:32 +0300

    #2 PublicEnum#id

--- src/main/java/ru/rnemykin/newsbot/model/enums/PublicEnum.java
+++ src/main/java/ru/rnemykin/newsbot/model/enums/PublicEnum.java
@@ -1,16 +1,29 @@
 package ru.rnemykin.newsbot.model.enums;
 
 public enum PublicEnum {
-    BEELIVE("https://vk.com/belgorod");
+    BEELIVE(36378934L, "https://vk.com/belgorod");
 
+    private Long id;
     private String url;
 
-    PublicEnum(String url) {
+    PublicEnum(Long id, String url) {
+        this.id = id;
         this.url = url;
     }
 
-
     public String url() {
         return url;
+    }
+    public Long id() {
+        return id;
+    }
+
+    public static PublicEnum fromId(Long id) {
+        for (PublicEnum publicEnum : PublicEnum.values()) {
+            if (publicEnum.id.equals(id)) {
+                return publicEnum;
+            }
+        }
+        throw new RuntimeException("Not found PublicEnum with id = " + id);
     }
 }
