7031d50094a0d42f7d240d9fe168e10a24f47157
rnemykin <nemykin.roman@gmail.con>
2017-12-17 20:55:32 +0300

    filter exists posts

--- src/main/java/ru/rnemykin/newsbot/repository/PostRepository.java
+++ src/main/java/ru/rnemykin/newsbot/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package ru.rnemykin.newsbot.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ru.rnemykin.newsbot.model.Post;
 
+import java.util.List;
+
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
+    List<Post> findAllByOwnerId(long ownerId, Pageable pageable);
 }
--- src/main/java/ru/rnemykin/newsbot/service/job/LoadNewsJob.java
+++ src/main/java/ru/rnemykin/newsbot/service/job/LoadNewsJob.java
@@ -3,6 +3,7 @@ package ru.rnemykin.newsbot.service.job;
 import com.vk.api.sdk.objects.wall.WallpostFull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.rnemykin.newsbot.model.Post;
@@ -17,13 +18,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static java.lang.Long.valueOf;
 import static java.time.Instant.ofEpochMilli;
 import static java.time.ZoneId.systemDefault;
 import static java.util.stream.Collectors.toList;
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Slf4j
 @Component
 public class LoadNewsJob {
+    private static final int POSTS_FETCH_SIZE = 10;
+
     private final VkService vkService;
     private final Map<CityEnum, Set<PublicEnum>> cityNews;
     private final PostRepository postRepository;
@@ -39,27 +44,34 @@ public class LoadNewsJob {
     @Scheduled(cron = "${job.loadNews.schedule}")
     public void loadNews() {
         cityNews.get(CityEnum.BELGOROD).forEach(cityPublic -> {
-            List<WallpostFull> vkWallPosts = vkService.getWallPosts(cityPublic.id());
+            List<WallpostFull> vkWallPosts = vkService.getWallPosts(cityPublic.id()); //todo pass posts_fetch_size
             log.info("retrieve {} vkWallPosts", vkWallPosts.size());
-            // todo filter existing
-            List<Post> posts = vkWallPosts.stream().map(p -> {
-                Post post = new Post();
-                post.setPostId(Long.valueOf(p.getId()));
-                post.setOwnerId(Long.valueOf(p.getOwnerId()));
-                post.setType(p.getPostType().getValue());
-                post.setPostDate(ofEpochMilli(p.getDate() * 1000L).atZone(systemDefault()).toLocalDateTime());
-                post.setText(p.getText().getBytes());
-                post.setLikesCount(p.getLikes().getCount() == null ? p.getLikes().getCount() : 0);
-                post.setViewsCount(p.getViews().getCount() == null ? p.getViews().getCount() : 0);
-                post.setRepostsCount(p.getReposts().getCount() == null ? p.getReposts().getCount() : 0);
-                post.setCommentsCount(p.getComments().getCount() == null ? p.getComments().getCount() : 0);
-                post.setIsPinned(Integer.valueOf(1).equals(p.getIsPinned()));
-                post.setCreateDate(LocalDateTime.now());
-                post.setStatus(PostStatusEnum.NEW);
-                return post;
-            }).collect(toList());
+            if(!isEmpty(vkWallPosts)) {
+                PageRequest pageRequest = new PageRequest(0, POSTS_FETCH_SIZE);
+                List<Post> publicPosts = postRepository.findAllByOwnerId(cityPublic.id(), pageRequest);
+                List<Post> posts = vkWallPosts.stream()
+                        .filter(vkPost -> publicPosts.stream().noneMatch(p -> p.getPostId().equals(valueOf(vkPost.getId()))))
+                        .map(this::mapToPost).collect(toList());
 
-            postRepository.save(posts);
+                postRepository.save(posts);
+            }
         });
+    }
+
+    private Post mapToPost(WallpostFull p) {
+        Post post = new Post();
+        post.setPostId(-1 * valueOf(p.getId()));
+        post.setOwnerId(-1 * valueOf(p.getOwnerId()));
+        post.setType(p.getPostType().getValue());
+        post.setPostDate(ofEpochMilli(p.getDate() * 1000L).atZone(systemDefault()).toLocalDateTime());
+        post.setText(p.getText().getBytes());
+        post.setLikesCount(p.getLikes().getCount() == null ? p.getLikes().getCount() : 0);
+        post.setViewsCount(p.getViews().getCount() == null ? p.getViews().getCount() : 0);
+        post.setRepostsCount(p.getReposts().getCount() == null ? p.getReposts().getCount() : 0);
+        post.setCommentsCount(p.getComments().getCount() == null ? p.getComments().getCount() : 0);
+        post.setIsPinned(Integer.valueOf(1).equals(p.getIsPinned()));
+        post.setCreateDate(LocalDateTime.now());
+        post.setStatus(PostStatusEnum.NEW);
+        return post;
     }
 }
