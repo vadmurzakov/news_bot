36d3e0cbe6370b4a8bc4eddb3e8054b5ddbc56ce
rnemykin <nemykin.roman@gmail.com>
2018-01-17 12:35:50 +0300

    message formatter  added date

--- src/main/java/ru/rnemykin/newsbot/service/MessageFormatter.java
+++ src/main/java/ru/rnemykin/newsbot/service/MessageFormatter.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Component;
 import ru.rnemykin.newsbot.model.Post;
 
 import java.text.MessageFormat;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class MessageFormatter {
-    private static final String MSG_FORMAT = "{0}\n\n<i>источник: {1}</i>";
+    private static final String MSG_FORMAT = "{0}\n\n<i>источник: {1}\n{2}</i>";
+    private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
 
     public String format(Post post) {
-        return MessageFormat.format(MSG_FORMAT, post.getTextAsString(), post.getPostPublic().url());
+        return MessageFormat.format(MSG_FORMAT, post.getTextAsString(), post.getPostPublic().url(), DTF.format(post.getPostDate()));
     }
 }
